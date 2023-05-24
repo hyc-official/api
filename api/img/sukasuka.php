7fa41f4dc709aa64dc6bd81a39831b51632c98ea
hyc1230 <i@heyc.eu.org>
2023-05-24 12:58:51 +0000

    Edit img src

--- api/img/sukasuka.php
+++ api/img/sukasuka.php
@@ -2,7 +2,8 @@
 
 $n = 125;
 $i = rand(1, $n);
-$p = "/src/img/sukasuka/$i.jpg";
+// $p = "/src/img/sukasuka/$i.jpg";
+$p = "https://raw.githubusercontent.com/hyc1230/Chtholly-img/main/$i.jpg";
 
 die(header('Location:' . $p));
 
