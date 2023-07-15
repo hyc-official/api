82a951fae96f56d7806efbf83f443b0535473da4
hyc1230 <i@heyc.eu.org>
2023-07-15 08:23:08 +0000

    Bing process: Not to generate temp.jpg

--- api/bing/process.py
+++ api/bing/process.py
@@ -3,25 +3,23 @@ import requests as rq
 import json
 from PIL import Image, ImageFilter
 from urllib.parse import urlparse, parse_qs
+import io
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         rp = rq.get(url='https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1')
         dt = rp.json()
         url = 'https://cn.bing.com' + dt['images'][0]['url']
-        rp = rq.get(url=url)
-        pth = "temp.jpg"
-        with open(pth, "wb+") as f:
-            f.write(rp.content)
-        img = Image.open(pth)
-        rad = int(parse_qs(urlparse(self.path).query).get("blurRadius")[0])
+        rp = rq.get(url)
+        img = Image.open(io.BytesIO(rp.content))
+        rad = int(parse_qs(urlparse(self.path).query).get('blurRadius', ['0'])[0])
         img = img.filter(ImageFilter.GaussianBlur(radius = rad))
-        img.save(pth)
-        with open(pth, "rb") as f:
-            self.send_response(200)
-            self.send_header('Content-type', 'image/jpeg')
-            self.end_headers()
-            self.wfile.write(f.read())
+        img_byte = io.BytesIO()
+        img.save(img_byte, format='JPEG')
+        self.send_response(200)
+        self.send_header('Content-type', 'image/jpeg')
+        self.end_headers()
+        self.wfile.write(img_byte.getvalue())
         return
 
 if __name__ == '__main__':
