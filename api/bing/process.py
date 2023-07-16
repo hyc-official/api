59e146378b1810f1fbd5408d23d5616469444cbe
hyc1230 <i@heyc.eu.org>
2023-07-16 02:03:04 +0000

    feat(Bing process): Add gray & rotate, blurRadius -> blur

--- api/bing/process.py
+++ api/bing/process.py
@@ -7,13 +7,23 @@ import io
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        qry = parse_qs(urlparse(self.path).query)
         rp = rq.get(url='https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1')
         dt = rp.json()
         url = 'https://cn.bing.com' + dt['images'][0]['url']
         rp = rq.get(url)
         img = Image.open(io.BytesIO(rp.content))
-        rad = int(parse_qs(urlparse(self.path).query).get('blurRadius', ['0'])[0])
-        img = img.filter(ImageFilter.GaussianBlur(radius = rad))
+
+        rad = int(qry.get('blur', ['0'])[0])
+        if rad != 0 :
+            img = img.filter(ImageFilter.GaussianBlur(radius = rad))
+        gry = int(qry.get('grey', ['0'])[0])
+        if gry != 0 :
+            img = img.convert('L')
+        rot = int(qry.get('rotate', ['0'])[0])
+        if rot != 0 :
+            img = img.rotate(rot)
+
         img_byte = io.BytesIO()
         img.save(img_byte, format='JPEG')
         self.send_response(200)
